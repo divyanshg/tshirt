11b93eb49ef1fb0d4836f10ec3f4f2bdf186b451
divyanshg <divyanshg809@gmail.com>
2020-06-28 01:13:55 +0530

    test

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://192.168.31.72:27017/";
+
 const options = {
     key: fs.readFileSync('../private.key'),
     cert: fs.readFileSync('../certificate.crt'),
@@ -15,10 +18,39 @@ var server = http.createServer(options, app).listen(30021, function () {
     console.log('Server ready ' + 30021);
 });
 
+var dataCamp;
+
+MongoClient.connect(url, {
+    useUnifiedTopology: true
+}, function (err, db) {
+    if (err) throw err;
+    dataCamp = db.db("fila_iot");
+});
+
+app.use(cors())
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/login.html')
 });
 
 app.get('/:user/:pass', (req, res) => {
-    res.json({status:200})
+    dataCamp.collection("users").find({
+        username: req.params.user,
+        password: req.params.pass
+    }).toArray((err, resp) => {
+        if (err) return err;
+
+        if (resp.length == 0) {
+            res.json({
+                status: 404
+            });
+            res.end();
+            return;
+        } else {
+            res.json({
+                status: 200
+            })
+            res.end()
+        }
+    })
 });
