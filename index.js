a3c3808f82f50cb8c1d64442deda17aeb357f362
divyanshg <divyanshg809@gmail.com>
2020-06-28 00:17:55 +0530

    test

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+var cors = require('cors')
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
-app.get('/', (req, res) => {
-    res.send('An alligator approaches!');
+const options = {
+    key: fs.readFileSync('../private.key'),
+    cert: fs.readFileSync('../certificate.crt'),
+    ca: [fs.readFileSync('../ca_bundle.crt')]
+};
+
+var http = require('https');
+
+var server = http.createServer(options, app).listen(30020, function () {
+    console.log('Server ready ' + 30020);
 });
 
-app.listen(30020, () => console.log('Gator app listening on port 3000!'));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/login.html')
+});
